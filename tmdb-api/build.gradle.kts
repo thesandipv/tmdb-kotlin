73c05755298acb59c1298d459698b21e0ef6da69
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2023-12-16 12:36:42 +0530

    replace deprecated function

--- tmdb-api/build.gradle.kts
+++ tmdb-api/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import java.util.Locale
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -121,7 +122,7 @@ tasks.withType<DependencyUpdatesTask> {
 }
 
 fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
     val regex = "^[0-9,.v-]+(-r)?$".toRegex()
     val isStable = stableKeyword || regex.matches(version)
     return isStable.not()
