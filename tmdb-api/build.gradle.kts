c6186646809c637cb0d421d38cce07b34d1d3139
Sandip Vaghela <12868991+thesandipv@users.noreply.github.com>
2024-07-07 12:57:48 +0530

    resolve build warnings

--- tmdb-api/build.gradle.kts
+++ tmdb-api/build.gradle.kts
@@ -88,6 +88,7 @@ kotlin {
         val iosSimulatorArm64Main by getting
 
         val iosMain by creating {
+            dependsOn(commonMain)
             iosX64Main.dependsOn(this)
             iosArm64Main.dependsOn(this)
             iosSimulatorArm64Main.dependsOn(this)
@@ -102,6 +103,7 @@ kotlin {
         val iosSimulatorArm64Test by getting
 
         val iosTest by creating {
+            dependsOn(commonTest)
             iosX64Test.dependsOn(this)
             iosArm64Test.dependsOn(this)
             iosSimulatorArm64Test.dependsOn(this)
